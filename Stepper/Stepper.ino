af3c04f0a341b7b42ad429f72ec916f8769f0501
christian maclean <christiankalmbachm@gmail.com>
2018-04-01 13:43:39 -0400

    stepper update

--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -5,15 +5,17 @@
 time_t start = time(NULL);
 
 /* Clockwise order */
-#define RED      _BV(PC0)
-#define BLUE     _BV(PC1)
-#define GREEN    _BV(PC2)
-#define BLACK    _BV(PC3)
+#define RED_X      _BV(PC0)
+#define BLUE_X     _BV(PC1)
+#define GREEN_X    _BV(PC2)
+#define BLACK_X    _BV(PC3)
 #define RED_Y    _BV(PC4)
 #define BLUE_Y   _BV(PC5)
 #define GREEN_Y  _BV(PC6)  
 #define BLACK_Y  _BV(PC7)
-#define CM       345
+#define CM       345 // equates to 1 cm
+#define DisX     CM*5.55; // equates to length of dispil in X
+#define DisY     CM*3; // equates to height of dispil in Y
 
 #define DELAY 1.5/* milliseconds between steps */
 
@@ -21,17 +23,20 @@ int main(void){
   DDRC = 0xff;    /* Enable output on all of the B pins */  
   PORTC = 0x00;            /* Set them all to 0v */  
   
-  
-  for(int i=0;i<5*CM;i++){/* main loop hddddere */    
-      PORTC = RED;
+
+
+  // This is for clockwise in X
+  for(int i=0;i<Column*CM;i++){  
+      PORTC = RED_X;
       _delay_ms(DELAY);  
-      PORTC = GREEN;    
+      PORTC = GREEN_X;    
       _delay_ms(DELAY); 
-      PORTC = BLUE;  
+      PORTC = BLUE_X;  
       _delay_ms(DELAY);    
-      PORTC = BLACK;    
+      PORTC = BLACK_X;    
       _delay_ms(DELAY);
   }
+  // This is for clockwise in Y
   for(int i=0;i<5*CM;i++){/* main loop hddddere */    
       PORTC = RED_Y;
       _delay_ms(DELAY);  
@@ -42,5 +47,46 @@ int main(void){
       PORTC = BLACK_Y;    
       _delay_ms(DELAY);
   }    
+
+// This is for counter clockwise in X
+for(int i=0;i<CM;i++){    
+      
+      PORTC = BLACK_X;
+      _delay_ms(DELAY);  
+      PORTC = BLUE_X;
+      _delay_ms(DELAY);  
+      PORTC = GREEN_X; 
+      _delay_ms(DELAY);  
+      PORTC = RED_X;   
+      _delay_ms(DELAY);
+  }
+
+// This is for counter clockwise in Y
+for(int i=0;i<5*CM;i++){/* main loop hddddere */    
+     
+      PORTC = BLACK_Y;
+      _delay_ms(DELAY);  
+      PORTC = BLUE_Y;
+      _delay_ms(DELAY);  
+      PORTC = GREEN_Y; 
+      _delay_ms(DELAY);  
+      PORTC = RED_Y;   
+      _delay_ms(DELAY);
+  }   
 }
 
+   
+}
+
+void moveX(row) 
+{
+  
+  
+  
+}
+void moveY(column)
+{
+  
+ 
+  
+} 
